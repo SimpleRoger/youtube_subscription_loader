2803504aaa28f7670bc970e3cb2c1a6cf8b0c562
Roger Tan <rtan4955@uni.sydney.edu.au>
2023-10-15 16:28:11 +1100

    changed to Index

--- pages/videos/index.js
+++ pages/videos/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Videos from "../components/Videos";
 import { useSelector } from "react-redux";
 
-export default function index() {
+export default function Index() {
   const user = useSelector((state) => state.user);
 
   return (
